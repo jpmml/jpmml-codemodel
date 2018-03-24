47e96ce13a7f99eac0d472d7211da9a47bf49e45
Villu Ruusmann <villu.ruusmann@gmail.com>
2018-03-24 09:28:48 +0200

    Added class JServiceConfigurationFile

---
+++ src/main/java/org/jpmml/codemodel/JServiceConfigurationFile.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2018 Villu Ruusmann
+ */
+package org.jpmml.codemodel;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Collection;
+import java.util.ServiceLoader;
+
+import com.sun.codemodel.JResourceFile;
+import com.sun.codemodel.JType;
+
+/**
+ * @see ServiceLoader
+ */
+public class JServiceConfigurationFile extends JResourceFile {
+
+	private JType service = null;
+
+	private Collection<? extends JType> serviceProviders = null;
+
+
+	public JServiceConfigurationFile(JType service, Collection<? extends JType> serviceProviders){
+		super(service.binaryName());
+
+		setService(service);
+		setServiceProviders(serviceProviders);
+	}
+
+	@Override
+	public void build(OutputStream os) throws IOException {
+		Writer writer = new OutputStreamWriter(os, "UTF-8");
+
+		String sep = "";
+
+		Collection<? extends JType> serviceProviders = getServiceProviders();
+		for(JType serviceProvider : serviceProviders){
+			writer.write(sep);
+
+			sep = "\n";
+
+			writer.write(serviceProvider.binaryName());
+		}
+
+		writer.flush();
+	}
+
+	public JType getService(){
+		return this.service;
+	}
+
+	private void setService(JType service){
+		this.service = service;
+	}
+
+	public Collection<? extends JType> getServiceProviders(){
+		return this.serviceProviders;
+	}
+
+	private void setServiceProviders(Collection<? extends JType> serviceProviders){
+		this.serviceProviders = serviceProviders;
+	}
+}
